43fe7665b89e0b3304e4c6ef18968fbe16cb81d3
Sepand <Sepand@SEPAND>
2023-11-01 02:24:37 +0330

    Add project files.

---
+++ fibonachi ramin/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonachi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 1;
+            int z;
+            int i;
+            int number;
+            Console.WriteLine("enter number:");
+            number = int.Parse(Console.ReadLine());
+            Console.Write(x + "" +y + "");
+            for (i = 2; i < number; i++)
+
+            {
+                z = x+ y;
+                Console.Write(z + "");
+                x = y;
+                y = z;
+
+            }
+            Console.WriteLine("is");
+            Console.ReadKey();
+            Console.Clear();
+           
+        }
+    }
+}
+//رامین کریمی
+//تمرین4
